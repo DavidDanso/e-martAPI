fd085d8a1d69e7a5060f6a5a51c9559ed81bbfc0
DavidDanso <desiingerboy@icloud.com>
2024-12-21 16:26:32 +0000

    feat(api): add serializers and create product_list endpoint

---
+++ api/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Product, Order, OrderItem
+
+class ProductSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Product
+        fields = ['name', 'description', 'price', 'stock']
+
+    def validate_price(self, value):
+        if value <= 0:
+            return serializers.ValidationError("Product price should be greater than 0.")
+        return value
