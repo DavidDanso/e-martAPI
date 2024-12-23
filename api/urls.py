0ce61316fea7f571eb823c795668e2d1e0492474
DavidDanso <desiingerboy@icloud.com>
2024-12-23 10:53:05 +0000

    feat(api): add product detail endpoint and include id in product serializer

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('products/', views.product_list, name='products')
+    path('products/', views.product_list),
+    path('products/<str:pk>/', views.get_product),
 ]
--- api/views.py
+++ api/views.py
@@ -5,8 +5,15 @@ from api.models import Product
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-
+@api_view(['GET'])
 def product_list(request):
     products = Product.objects.all()
     serializer = ProductSerializer(products, many=True)
-    return JsonResponse({'data': serializer.data})
+    return Response({'data': serializer.data})
+
+
+@api_view(['GET'])
+def get_product(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    serializer = ProductSerializer(product)
+    return Response({'data': serializer.data})
