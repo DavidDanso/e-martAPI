576d8d2ac0a1c76984e6e2e9050caa00e3f76c10
DavidDanso <desiingerboy@icloud.com>
2024-12-31 11:20:02 +0000

    feat(api): add order list endpoint and update serializers for order and order items

--- api/serializers.py
+++ api/serializers.py
@@ -11,3 +11,18 @@ class ProductSerializer(serializers.ModelSerializer):
         if value <= 0:
             return serializers.ValidationError("Product price should be greater than 0.")
         return value
+
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = ['product', 'quantity']
+
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True, read_only=True)
+    class Meta:
+        model = Order
+        fields = ['order_id', 'created_at', 'user', 'status', 'items']
